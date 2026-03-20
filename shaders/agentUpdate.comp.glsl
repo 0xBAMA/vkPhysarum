ba993bad4e1431a2c1ec55f82e7b9cd4b8bef87e
jb <jb239812@ohio.edu>
2026-03-20 17:38:45 -0400

    rng header + some of the basic outline for the update shader

--- shaders/agentUpdate.comp.glsl
+++ shaders/agentUpdate.comp.glsl
@@ -4,6 +4,7 @@
 #extension GL_EXT_buffer_reference : require
 //=========================================================
 #include "common.h"
+#include "random.h"
 //=========================================================
 // size of a workgroup for compute
 layout ( local_size_x = 16 ) in;
@@ -15,6 +16,46 @@ layout ( local_size_x = 16 ) in;
 	MOVE - you have a new direction, take a step that way
 	DEPOSIT (now happens via raster)
 */
+
+//=========================================================
+#define WANGSEED PushConstants.wangSeed
+#define MYAGENT agents[ gl_GlobalInvocationID.x ]
+#define BUFFERSIZE vec2( GlobalData.floatBufferResolution )
+//=========================================================
+vec2 rotate ( const vec2 v, const float a ) {
+	// takes argument in radians
+	const float s = sin( a );
+	const float c = cos( a );
+	const mat2 m = mat2( c, -s, s, c );
+	return m * v;
+}
+vec2 wrap ( vec2 pos ) {
+	const ivec2 iS = ivec2( BUFFERSIZE );
+	if ( pos.x >= iS.x ) pos.x -= iS.x;
+	if ( pos.x < 0.0f ) pos.x += iS.x;
+	if ( pos.y >= iS.y ) pos.y -= iS.y;
+	if ( pos.y < 0.0f ) pos.y += iS.y;
+	return pos;
+}
+//=========================================================
 void main () {
-	agents[ gl_GlobalInvocationID.x ].velocity += 0.1f;
+	seed = WANGSEED + gl_GlobalInvocationID.x * 69420;
+
+	if ( PushConstants.operation == -1 ) {
+		// generating new, random values
+
+	// do the regular agent update...
+		// sense taps, reading from the pheremone buffer
+
+		// turn decision, based on the sense readings
+
+		// move the agent based on the current velocity
+
+		// wrap the position to keep it in-bounds for the raster process
+		if ( clamp( MYAGENT.position, vec2( 0.0f ), BUFFERSIZE ) != MYAGENT.position ) {
+			MYAGENT.position = wrap( MYAGENT.position );
+		}
+	} else {
+
+	}
 }
